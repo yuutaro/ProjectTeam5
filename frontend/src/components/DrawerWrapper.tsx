8a73e723709e4f1fb94aed530b1b36bf33f58fe0
yutaro-desktop <yuutaro.mikasa@gmail.com>
2024-10-07 02:42:32 +0900

    frontend/ Drawer実装

---
+++ frontend/src/components/DrawerWrapper.tsx
@@ -0,0 +1,71 @@
+import { AppProps } from 'next/app'
+import React from 'react'
+
+interface DrawerWrapperProps {
+  Component: AppProps['Component']
+  pageProps: AppProps['pageProps']
+}
+
+export const DrawerWrapper = ({ Component, pageProps }: DrawerWrapperProps) => {
+  return (
+    <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="navbar bg-base-300 w-full">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="mx-2 flex-1 px-2">馬券当たる君</div>
+          <div className="hidden flex-none lg:block">
+            <ul className="menu menu-horizontal">
+              {/* Navbar menu content here */}
+              <li>
+                <a>Navbar Item 1</a>
+              </li>
+              <li>
+                <a>Navbar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Page content here */}
+        <Component {...pageProps} />
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
-import { Header } from '@/components/header'
+import { DrawerWrapper } from '@/components/DrawerWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <DrawerWrapper Component={Component} pageProps={pageProps} />
+      {/* <Component {...pageProps} /> */}
     </>
   )
 }

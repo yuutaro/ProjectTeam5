9ba0c40eb4850650e9229e7ab17dae88c71eedbb
yuutaro-v3 <yuutaro.mikasa@gmail.com>
2024-10-10 16:59:11 +0900

    いったんデザインをリセット

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,13 +10,7 @@ const Index: React.FC = () => {
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
 
-  return (
-    <div style={{ backgroundImage: `url("/image/東京競馬場_-_panoramio_(4).jpg")` }}>
-      <h1 className="mt-28 text-center text-8xl animate-fade-up animate-once animate-ease-in-out">馬券あたる君</h1>
-      <div>health check : {data.healthcheck}</div>
-      <div className="p-96"></div>
-    </div>
-  )
+  return <div>health check : {data.healthcheck}</div>
 }
 
 export default Index

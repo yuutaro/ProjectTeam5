4995c492f30fb83f88c2f4fd6a52d4664bb8cc8a
yuutaro-v3 <yuutaro.mikasa@gmail.com>
2024-10-29 08:52:23 +0900

    <select>要素のvalue属性に入れる値はuseStateなどで状態管理されていないといけないためそのように修正

--- frontend/src/components/ThemeSelector.tsx
+++ frontend/src/components/ThemeSelector.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setTheme, Theme, themes } from '@/redux/slices/themeSlice'
 
 export const ThemeSelector: React.FC = () => {
   const dispatch = useDispatch()
+  const [selectedTheme, setSelectedTheme] = useState('') // 初期値を空に設定
+
   const toggleTheme = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setTheme(e.target.value as Theme))
+    const theme = e.target.value as Theme
+    setSelectedTheme(theme)
+    dispatch(setTheme(theme))
   }
 
   return (
-    <select className="select w-full max-w-xs" onChange={toggleTheme}>
-      <option disabled selected>
+    <select className="select w-full max-w-xs" value={selectedTheme} onChange={toggleTheme}>
+      <option disabled value="">
         Theme
       </option>
       {themes.map((theme) => (
-        <option key={theme}>{theme}</option>
+        <option key={theme} value={theme}>
+          {theme}
+        </option>
       ))}
     </select>
   )

cc8b77e8806790d37936dbb59333f7c8ba5f109d
yuutaro-v3 <yuutaro.mikasa@gmail.com>
2024-10-29 10:05:46 +0900

    カスタムツールチップを追加（共通コンポーネント化はしておらず直書き）

--- frontend/src/components/charts/TimeChart.tsx
+++ frontend/src/components/charts/TimeChart.tsx
@@ -14,11 +14,27 @@ const TimeChart: React.FC = () => {
         <Line type="monotone" dataKey="data2" stroke="#82ca9d" dot={false} />
         <XAxis dataKey="time" />
         <YAxis />
-        <Tooltip />
+        <Tooltip content={<CustomToolTip active={false} payload={[]} label={''} />} cursor={false} />
         <Legend />
       </LineChart>
     </>
   )
+}
+
+const CustomToolTip: React.FC<{ active: boolean; payload: any[]; label: string }> = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    // 100分率にして二桁まで表示
+    const prob1 = (payload[0].value * 100).toFixed(2)
+    const prob2 = (payload[1].value * 100).toFixed(2)
+    return (
+      <div className="bg-base-100 p-2 rounded-lg shadow-md">
+        <p className="prose prose-h6">{`time: ${label} s`}</p>
+        <p className="prose prose-p">{`data1: ${prob1}%`}</p>
+        <p className="prose prose-p">{`data2: ${prob2}%`}</p>
+      </div>
+    )
+  }
+  return null
 }
 
 export default TimeChart

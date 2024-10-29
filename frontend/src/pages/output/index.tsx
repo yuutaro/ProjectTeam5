ac0ad9c8bafa6eb58cdec33b42ac6f905263b737
yuutaro-v3 <yuutaro.mikasa@gmail.com>
2024-10-29 15:28:13 +0900

    ダイナミックインポートでcsrのみのコンポーネントを作成

--- frontend/src/pages/output/index.tsx
+++ frontend/src/pages/output/index.tsx
@@ -4,7 +4,16 @@ import React from 'react'
 // ssrを無効化するためにはダイナミックインポートを行えばよい。これによりssrを無効化し、クライアントサイドでのみレンダリングされる。
 // rechartsのコンポーネントはSSRすると挙動がおかしくなるため、ダイナミックインポートを行う。
 // また表示するデータは今後APIから取得するため、いずれにせよクライアントサイドでのみレンダリングする必要がある。
-const TimeChart = dynamic(() => import('@/components/charts/TimeChart'), { ssr: false })
+const TimeChart = dynamic(() => import('@/components/charts/TimeChart'), {
+  ssr: false,
+  loading: () => {
+    return (
+      <div className="h-[400px] w-[800px] bg-base-100 rounded-xl flex justify-center items-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    )
+  },
+})
 
 const Index: React.FC = () => {
   return (

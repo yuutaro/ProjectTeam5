b2faa4a4250129a20e89e0121badedab3f8bac0c
yuutaro-v3 <yuutaro.mikasa@gmail.com>
2024-11-05 07:53:10 +0900

    rechartの表示を一部変更

--- frontend/src/components/charts/TimeChart.tsx
+++ frontend/src/components/charts/TimeChart.tsx
@@ -1,22 +1,20 @@
-import { CartesianGrid, Legend, Line, LineChart, ReferenceDot, Tooltip, XAxis, YAxis } from 'recharts'
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { probability_distribution_data_1 } from './probability_distribution_data'
 
 const TimeChart: React.FC = () => {
   return (
     <>
-      <LineChart width={730} height={400} data={probability_distribution_data_1}>
-        <CartesianGrid
-          verticalCoordinatesGenerator={(props) => {
-            return props.width > 450 ? [150, 300, 450] : [200, 400]
-          }}
-        />
-        <Line type="monotone" dataKey="data1" stroke="#8884d8" dot={false} />
-        <Line type="monotone" dataKey="data2" stroke="#82ca9d" dot={false} />
-        <XAxis dataKey="time" />
-        <YAxis />
-        <Tooltip content={<CustomToolTip active={false} payload={[]} label={''} />} cursor={false} />
-        <Legend />
-      </LineChart>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={probability_distribution_data_1}>
+          <CartesianGrid strokeDasharray={'3 3'} />
+          <Line type="monotone" dataKey="data1" stroke="#8884d8" dot={false} />
+          <Line type="monotone" dataKey="data2" stroke="#82ca9d" dot={false} />
+          <XAxis dataKey="time" interval={29} />
+          <YAxis />
+          <Tooltip content={<CustomToolTip active={false} payload={[]} label={''} />} cursor={false} />
+          <Legend />
+        </LineChart>
+      </ResponsiveContainer>
     </>
   )
 }
@@ -29,8 +27,8 @@ const CustomToolTip: React.FC<{ active: boolean; payload: any[]; label: string }
     return (
       <div className="bg-base-100 p-2 rounded-lg shadow-md">
         <p className="prose prose-h6">{`time: ${label} s`}</p>
-        <p className="prose prose-p">{`data1: ${prob1}%`}</p>
-        <p className="prose prose-p">{`data2: ${prob2}%`}</p>
+        <p className="prose prose-p">{`data1: ${prob1} %`}</p>
+        <p className="prose prose-p">{`data2: ${prob2} %`}</p>
       </div>
     )
   }
--- frontend/src/pages/output/index.tsx
+++ frontend/src/pages/output/index.tsx
@@ -15,11 +15,43 @@ const TimeChart = dynamic(() => import('@/components/charts/TimeChart'), {
   },
 })
 
+type HorseData = {
+  // 馬名　ex. メジロマックイーン
+  horseName: string
+  // 到着時間の確率分布　index: t[s] value: P(t)　ex. [0.1, 0.2, 0.3, 0.4, ...]
+  probabilityDistribution: number[]
+  // 平均到着時間
+  average: number
+  // 到着時間の分散
+  dispersion: number
+}
+
+type ServerResponse = {
+  // ここからデータベースのカラム
+  // Date,Track,Weather,Race Number,Distance,Condition,Time,Jockey,Trainer,first_horses,second_horses,third_horses
+  date: string // 日付 ex. 2021-01-01T12:00:00Z <- GMT-09:00
+  track: string // 競馬場 ex. 札幌
+  weather: string // 天候 ex. 晴
+  raceNumber: number // レース番号 ex. 1
+  distance: number // 距離 ex. 1200
+  condition: string // 馬場状態 ex. 良
+  // 個別の馬のデータ
+  horses: HorseData[]
+  // オッズなどの加工データ
+  
+}
+const data = [{ horseName: 'メジロマックイーン' }]
+
 const Index: React.FC = () => {
   return (
     <>
-      <div className="h-32 w-full bg-base-100"></div>
-      <TimeChart />
+      <div className="h-28 w-full bg-base-100"></div>
+      <div className="flex flex-col lg:flex-row">
+        <TimeChart />
+        <div className="h-64 bg-base-200 w-full">
+          <h3 className=" font-notosans">確率予測</h3>
+        </div>
+      </div>
     </>
   )
 }
